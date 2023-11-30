39693b684827ed42b4a964dd187d86af26b200bf
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2023-11-30 20:13:53 +0800

    it-should be className

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -5,7 +5,7 @@ import styles from '../ui/dashboard/dashboard.module.css'
 const Layout = ({ children }) => {
   return (
     <div className={styles.container}>
-      <div style={styles.menu}>
+      <div className={styles.menu}>
         <Sidebar />
       </div>
       <div className={styles.content}>
